91de00b9a052bd4ba30f7dae250fbd3643890c24
Dixit020 <prajapatidixit239@gmail.com>
2025-06-23 07:53:30 +0530

    Update db.php

--- db.php
+++ db.php
@@ -4,7 +4,7 @@ $user = "root";
 $pass = "";
 $db = "news17";
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_connect("sql206.infinityfree.com", "if0_39296873", "12ab34", "if0_39296873_blogs");
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
